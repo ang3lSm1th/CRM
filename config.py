c065ea276e83ebdc38a340e8abe31e327c07fa25
ang3lSm1th <pocito700@gmail.com>
2025-09-11 09:55:32 -0500

    config.py

--- config.py
+++ config.py
@@ -2,8 +2,8 @@ import os
 
 class Config:
     SECRET_KEY = os.getenv("SECRET_KEY", "cambia_esta_clave")
-    MYSQL_HOST = os.getenv("MYSQL_HOST", "127.0.0.1")
-    MYSQL_USER = os.getenv("MYSQL_USER", "root")
-    MYSQL_PASSWORD = os.getenv("MYSQL_PASSWORD", "")
-    MYSQL_DB = os.getenv("MYSQL_DB", "crm_orbes")
+    MYSQL_HOST = os.getenv("MYSQL_HOST", "auth-db1896.hstgr.io")
+    MYSQL_USER = os.getenv("MYSQL_USER", "u349183440_orbesagricola")
+    MYSQL_PASSWORD = os.getenv("MYSQL_PASSWORD", "Orbesagricola25")
+    MYSQL_DB = os.getenv("MYSQL_DB", "u349183440_crm_orbes")
     MYSQL_CURSORCLASS = "DictCursor"
